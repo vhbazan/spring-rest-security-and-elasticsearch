58ca45fe0094cee89d6acfef8d21bf2f31ba06d7
vbazanm <victor_bazan_milla@optum.com>
2019-12-22 20:16:30 +0000

    Add post controller

---
+++ src/main/java/com/vhbazanm/App/controller/PostController.java
@@ -0,0 +1,24 @@
+package com.vhbazanm.App.controller;
+
+import com.vhbazanm.App.com.vhbazanm.model.Post;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class PostController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/post")
+    public Post post() {
+        String[] tags = {"health", "life"};
+        return new Post("abc", "first post", "description of the post", tags, "vhbazanm", new Date());
+    }
+
+}
+
+
