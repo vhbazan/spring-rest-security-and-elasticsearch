3501b3de60be76a72a48813a5bc69e970f068362
vbazanm <victor_bazan_milla@optum.com>
2019-12-22 20:16:09 +0000

    First Spring boot application files

---
+++ src/test/java/com/vhbazanm/App/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vhbazanm.App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
